a3d93632087d0ac7ec47208d773dedb20f56cc38
Johann Klemmack <johann.klemmack@gmail.com>
2016-06-04 13:10:09 -0700

    Got local copy building

---
+++ MechJeb2Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MuMech;
+
+namespace MechJeb2Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var x = new DebugIcoSphere(UnityEngine.Color.red, true);
+        }
+    }
+}
